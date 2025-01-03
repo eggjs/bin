515614a2338381c5a5878c2af794a9b1964a2e90
fengmk2 <fengmk2@gmail.com>
2025-01-03 10:20:07 +0800

    fix: auto import tsconfig-paths/register.js (#282)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    	- Added a new `/foo` route in the application.
    	- Introduced a `Foo` class with a `bar` method.
    	- Enhanced TypeScript path resolution configuration.
    
    - **Tests**
    - Updated test cases to cover new functionality, including the `/foo`
    endpoint.
    - Improved test suite with additional assertions and updated expected
    outputs.
    	- Updated mock dependency to version 6.
    
    - **Chores**
    - Updated TypeScript compiler settings to target ES2022 and refined
    module resolution strategies.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- test/fixtures/example-ts/test/index.test.ts
+++ test/fixtures/example-ts/test/index.test.ts
@@ -1,12 +1,26 @@
-// @ts-ignore
+import { strict as assert } from 'node:assert';
 import { app } from '@eggjs/mock/bootstrap';
+import { Foo } from '@/module/foo'
 
-describe('test/index.test.ts', () => {
+describe('example-ts/test/index.test.ts', () => {
   it('should work', async () => {
     await app.ready();
     await app.httpRequest()
       .get('/')
       .expect('hi, egg')
       .expect(200);
+  });
+
+  it('should paths work', async () => {
+    await app.ready();
+    await app.httpRequest()
+      .get('/foo')
+      .expect('bar')
+      .expect(200);
+  });
+
+  it('should auto import tsconfig-paths/register', async () => {
+    const instance = new Foo();
+    assert.equal(instance.bar(), 'bar');
   });
 });
